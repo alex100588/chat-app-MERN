00d56862914b3ad5684ee9a3b027308e2b140983
alex100588 <alexandru_ionasc@yahoo.com>
2025-04-10 16:36:59 +0300

    login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,6 +5,9 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { email, fullName, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
     if (password.length < 6) {
       return res
         .status(400)
@@ -25,31 +28,55 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-        // generate jwt token
-        generateToken(newUser._id, res)
-        // save the user to the database
-        await newUser.save()
+      // generate jwt token
+      generateToken(newUser._id, res);
+      // save the user to the database
+      await newUser.save();
 
-        // success message sent with new user created
-        res.status(201).json({
-            _id:newUser._id,
-            fullName: newUser.fullName,
-            email: newUser.email,
-            profilePic: newUser.profilePic
-        })
+      // success message sent with new user created
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
       return res.status(400).json({ message: "Email already exists" });
     }
-
   } catch (error) {
     console.log("Error in the signup controller", error.message);
-    res.staus(500).json({ message: "Internal server error" })
-    
+    res.staus(500).json({ message: "Internal server error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("signup route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPassworCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPassworCorrect) {
+      return res.status(400).json({ message: "Invalid credentials " });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error"})
+  }
 };
 
 export const logout = (req, res) => {
