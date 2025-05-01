f8541e242a6f766dd49f3e5005aa54c77af86c8d
alex100588 <alexandru_ionasc@yahoo.com>
2025-05-01 15:20:56 +0300

    connect/disconnect

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,20 +1,25 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
-export const useAuthStore = create((set) => ({
+const BASE_URL="http://localhost:5001"
+
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
   onlineUsers: [],
+  socket: null,
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       console.log(res);
       set({ authUser: res.data });
+      get().connectSocket()
     } catch (error) {
       console.log("Error in checkAuth:", error);
       set({ authUser: null });
@@ -29,6 +34,8 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
+      get().connectSocket()
+
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -41,6 +48,8 @@ export const useAuthStore = create((set) => ({
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
+      get().disconnectSocket()
+
     } catch (error) {
       toast.error(error.response.data.message)
     }
@@ -74,6 +83,20 @@ export const useAuthStore = create((set) => ({
       set({ isUpdatingProfile: false });
     }
   },
+
+  connectSocket: ()=>{
+    const {authUser} = get()
+    if(!authUser || get().socket?.connected) return
+
+    const socket = io(BASE_URL)
+    socket.connect()
+
+    set({socket:socket})
+  },
+
+  disconnectSocket: ()=>{
+    if(get().socket?.connected) get().socket.disconnect()
+  }
   
   
 }));
