51a1304d4bac44d6fd7a18b86d3b4770172e554a
alex100588 <alexandru_ionasc@yahoo.com>
2025-05-08 20:03:16 +0300

    route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { protectedRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/users", protectedRoute, getUsersForSidebar)
-router.get("/:id", protectedRoute, getMessages)
+router.get("/users", protectedRoute, getUsersForSidebar);
+router.get("/:id", protectedRoute, getMessages);
+router.post("/send/:id", protectedRoute, sendMessage);
 
-router.post("/send/:id", protectedRoute, sendMessage)
-
-export default router
+export default router;
