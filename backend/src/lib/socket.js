48670c8d9c0d0b8ef8537d1246e5f713704dba72
alex100588 <alexandru_ionasc@yahoo.com>
2025-05-09 15:54:31 +0300

    test

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,31 +8,28 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"],
-    credentials: true
   },
 });
 
-export const getReceiverSocketId = (userId) => {
-  return userSocketMap[userId]
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
 }
 
-// Used to store the online users
-const userSocketMap = {}
+// used to store online users
+const userSocketMap = {}; 
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId
-  if(userId) userSocketMap[userId] =socket.id
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-  // used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete(userSocketMap[userId])
-    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
